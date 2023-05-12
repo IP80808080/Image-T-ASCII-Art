19da964a2bcea14f853df91868c4cea249a4a16e
KamleshKasambe <kasambe.kamlesh1@gmail.com>
2023-05-12 22:12:51 +0530

    Commit message

---
+++ main.py
@@ -0,0 +1,3 @@
+import pywhatkit as kit
+
+new = kit.image_to_ascii_art("Ayaka.jpg", "Ayaka")
---
+++ main2.py
@@ -0,0 +1,3 @@
+import ascii_magic
+out = ascii_magic.from_image("K:\Projects\Programming\Python\Project\Image and Python\Convert Image into ASCII Art\Ayaka.jpg", columns=300, char="#")
+ascii_magic.to_terminal(out)
